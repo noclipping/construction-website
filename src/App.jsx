0861428bc62c59f891f4b7dd180e1296bbf9226f
noclipping <aresous@gmail.com>
2023-02-09 22:45:37 -0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Hero from "./components/Hero";
+import Stats from "./components/Stats";
+import Reviews from "./components/Reviews";
+import Navbar from "./components/Navbar";
+export default function App() {
+return(
+  <div>
+    <Navbar/>
+    <Hero/>
+    <Stats/>
+    <Reviews/>
+  </div>
+)
+  
+}
